cb86dd9f7ef795f95addb9e18a69f5c21bc0439c
Balji Maurya <genius.abhishek.sir@gmail.com>
2025-05-06 09:20:19 +0530

    final

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // Required to generate 'out/' folder
   images: {
     remotePatterns: [
-      { hostname: 'api.mapbox.com' },
-      { hostname: 'res.cloudinary.com' },
-      { hostname: 'lh3.googleusercontent.com' },
+      { hostname: "api.mapbox.com" },
+      { hostname: "res.cloudinary.com" },
+      { hostname: "lh3.googleusercontent.com" },
     ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
