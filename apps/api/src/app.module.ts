c7a09d836a56f6175608587f8ab64cad3948b8f9
Balji Maurya <genius.abhishek.sir@gmail.com>
2025-05-08 11:25:35 +0530

    final

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,30 +1,30 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { GraphQLModule } from '@nestjs/graphql'
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
-import { join } from 'path'
-import { ConfigModule } from '@nestjs/config'
-import { PrismaModule } from './common/prisma/prisma.module'
-import { UsersModule } from './models/users/users.module'
-import { JwtModule } from '@nestjs/jwt'
-import { AdminsModule } from './models/admins/admins.module'
-import { CustomersModule } from './models/customers/customers.module'
-import { ManagersModule } from './models/managers/managers.module'
-import { ValetsModule } from './models/valets/valets.module'
-import { CompaniesModule } from './models/companies/companies.module'
-import { GaragesModule } from './models/garages/garages.module'
-import { AddressesModule } from './models/addresses/addresses.module'
-import { SlotsModule } from './models/slots/slots.module'
-import { BookingsModule } from './models/bookings/bookings.module'
-import { ValetAssignmentsModule } from './models/valet-assignments/valet-assignments.module'
-import { BookingTimelinesModule } from './models/booking-timelines/booking-timelines.module'
-import { ReviewsModule } from './models/reviews/reviews.module'
-import { VerificationsModule } from './models/verifications/verifications.module'
-import { StripeModule } from './models/stripe/stripe.module'
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { join } from "path";
+import { ConfigModule } from "@nestjs/config";
+import { PrismaModule } from "./common/prisma/prisma.module";
+import { UsersModule } from "./models/users/users.module";
+import { JwtModule } from "@nestjs/jwt";
+import { AdminsModule } from "./models/admins/admins.module";
+import { CustomersModule } from "./models/customers/customers.module";
+import { ManagersModule } from "./models/managers/managers.module";
+import { ValetsModule } from "./models/valets/valets.module";
+import { CompaniesModule } from "./models/companies/companies.module";
+import { GaragesModule } from "./models/garages/garages.module";
+import { AddressesModule } from "./models/addresses/addresses.module";
+import { SlotsModule } from "./models/slots/slots.module";
+import { BookingsModule } from "./models/bookings/bookings.module";
+import { ValetAssignmentsModule } from "./models/valet-assignments/valet-assignments.module";
+import { BookingTimelinesModule } from "./models/booking-timelines/booking-timelines.module";
+import { ReviewsModule } from "./models/reviews/reviews.module";
+import { VerificationsModule } from "./models/verifications/verifications.module";
+import { StripeModule } from "./models/stripe/stripe.module";
 
 // Todo: Move this to util lib.
-const MAX_AGE = 24 * 60 * 60
+const MAX_AGE = 24 * 60 * 60;
 
 @Module({
   imports: [
@@ -37,11 +37,11 @@ const MAX_AGE = 24 * 60 * 60
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       introspection: true,
-      fieldResolverEnhancers: ['guards'],
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      //   buildSchemaOptions: {
-      //      numberScalarMode: 'integer',
-      //   },
+      fieldResolverEnhancers: ["guards"],
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      buildSchemaOptions: {
+        numberScalarMode: "integer",
+      },
     }),
 
     PrismaModule,
