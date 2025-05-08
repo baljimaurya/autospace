e2984e1a379d7f9b06992db9fa31e6027e512877
Balji Maurya <genius.abhishek.sir@gmail.com>
2025-05-08 09:13:53 +0530

    final

--- apps/api/src/models/stripe/dto/create-stripe-session.dto.ts
+++ apps/api/src/models/stripe/dto/create-stripe-session.dto.ts
@@ -1,5 +1,5 @@
+import { TotalPrice } from "./../../../../../../libs/util/types";
 import { CreateBookingInput } from "src/models/bookings/graphql/dtos/create-booking.input";
-import { TotalPrice } from "../../../../../../libs/util/types";
 
 export class CreateStripeDto {
   uid: string;
